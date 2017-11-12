456289d66a6a7754146fc10c773bdcbd74e91d52
eparlato <eparlato82@gmail.com>
2017-11-12 12:42:20 +0100

    tests refactoring.

--- test/js/orders_processing_spec.js
+++ test/js/orders_processing_spec.js
@@ -3,44 +3,44 @@ describe('Orders processing', function () {
     var priceCalculator = new PriceCalculator();
     var taxCalculator = new TaxCalculator();
     var discountCalculator = new DiscountCalculator();
+    var input;
+    var orderProcessor = new OrderProcessor(priceCalculator, taxCalculator, discountCalculator);
+    var total_price;
 
     it('zero items - slice 0', function () {
 
-        var input = {
+        input = {
             tot_items: 0,
             price_per_item: 0,
             state_code: ""
         };
 
-        var orderProcessor = new OrderProcessor(priceCalculator, taxCalculator, discountCalculator);
-        var total_price = orderProcessor.process(input);
+        total_price = orderProcessor.process(input);
         
         expect(total_price).toEqual('$0');
     });
 
     it('1 item with a fixed price - slice 1', function () {
 
-        var input = {
+        input = {
             tot_items: 1,
             price_per_item: 5,
             state_code: ""
         };
 
-        var orderProcessor = new OrderProcessor(priceCalculator, taxCalculator, discountCalculator);
-        var total_price = orderProcessor.process(input);
+        total_price = orderProcessor.process(input);
         
         expect(total_price).toEqual('$5');
     });
 
     it('2 items, total with no taxes nor discount - slice 2', function () {
-        var input = {
+        input = {
             tot_items: 2,
             price_per_item: 7,
             state_code: ""
         };
 
-        var orderProcessor = new OrderProcessor(priceCalculator, taxCalculator, discountCalculator);
-        var total_price = orderProcessor.process(input);
+        total_price = orderProcessor.process(input);
         
         expect(total_price).toEqual('$14');
     });
