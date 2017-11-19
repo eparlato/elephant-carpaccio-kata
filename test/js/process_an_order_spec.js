acc8030916b55d160cb24297cc46fb1386aee030
eparlato <eparlato82@gmail.com>
2017-11-19 11:16:35 +0100

    slice 6 implemented.

--- test/js/process_an_order_spec.js
+++ test/js/process_an_order_spec.js
@@ -8,7 +8,7 @@ describe('When I process an order', function () {
     var orderProcessor = new OrderProcessor(priceCalculator, taxCalculator, discountCalculator);
     var total_price;
 
-    it('total price is $0 if there are no items', function () {
+    it('the total price is $0 if there are no items', function () {
 
         input = {
             tot_items: 0,
@@ -21,7 +21,7 @@ describe('When I process an order', function () {
         expect(total_price).toEqual('$0');
     });
 
-    it('total price is $5 if I have 1 item whose price is 5 - slice 1', function () {
+    it('the total price is $5 if I have 1 item whose price is 5 - slice 1', function () {
 
         input = {
             tot_items: 1,
@@ -34,7 +34,7 @@ describe('When I process an order', function () {
         expect(total_price).toEqual('$5');
     });
 
-    it('total price is $14 if I have 2 items and price_per_item is 7 (no taxes no discount) - slice 2', function () {
+    it('the total price is $14 if I have 2 items and price_per_item is 7 (no taxes no discount) - slice 2', function () {
         input = {
             tot_items: 2,
             price_per_item: 7,
@@ -46,7 +46,7 @@ describe('When I process an order', function () {
         expect(total_price).toEqual('$14');
     });
 
-    it('total price is $20 if I have 5 items and price_per_item is 4 (no taxes no discount) - slice 3', function () {
+    it('the total price is $20 if I have 5 items and price_per_item is 4 (no taxes no discount) - slice 3', function () {
         input = {
             tot_items: 5,
             price_per_item: 4,
@@ -58,7 +58,7 @@ describe('When I process an order', function () {
         expect(total_price).toEqual('$20');
     });
 
-    it('tax value is 137 (6.85%) if state_code is UT - slice 4', function () {
+    it('the tax value is 137 (6.85%) if state_code is UT - slice 4', function () {
 
         input = {
             tot_items: 2,
@@ -71,7 +71,7 @@ describe('When I process an order', function () {
         expect(total_price).toEqual('$2137');
     });
 
-    it('tax rate is 160 if state code is NV - slice 5', function () {
+    it('the tax rate is 160 if state code is NV - slice 5', function () {
 
         input = {
             tot_items: 2,
@@ -84,7 +84,7 @@ describe('When I process an order', function () {
         expect(total_price).toEqual('$2160');
     });
 
-    it('tax rate is 120 if total price is 1500 and the state code is NV', function () {
+    it('the tax rate is 120 if total price is 1500 and the state code is NV', function () {
 
         input = {
             tot_items: 3,
@@ -95,5 +95,28 @@ describe('When I process an order', function () {
         total_price = orderProcessor.process(input);
 
         expect(total_price).toEqual('$1620');
+    });
+
+    it('the tax rate is different whether the state code is different - slice 6', function() {
+        input = {
+            tot_items: 5,
+            price_per_item: 20
+        };
+
+        input.state_code = 'UT';
+        expect(orderProcessor.process(input)).toEqual('$106.85');
+
+        input.state_code = 'NV';
+        expect(orderProcessor.process(input)).toEqual('$108');
+
+        input.state_code = 'TX';
+        expect(orderProcessor.process(input)).toEqual('$106.25');
+
+        input.state_code = 'AL';
+        expect(orderProcessor.process(input)).toEqual('$104');
+
+        input.state_code = 'CA';
+        expect(orderProcessor.process(input)).toEqual('$108.25');
+        
     });
 });
